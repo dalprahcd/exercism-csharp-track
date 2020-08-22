c369895577d170bc1fefa244ae5f5516123b4103
dalprahcd <dalpra.hcd@gmail.com>
2020-08-22 09:51:06 -0300

    ADDED: Isogram exercise solution.

---
+++ isogram/Isogram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        if (word == null)
+        {
+            return false;
+        }
+
+        if (word.All(char.IsWhiteSpace))
+        {
+            return true;
+        }
+
+        var onlyLetters = word
+                            .Where(char.IsLetter)
+                            .Select(c => c.ToString());
+
+        return onlyLetters.Count() == onlyLetters
+                                        .Distinct(StringComparer.OrdinalIgnoreCase)
+                                        .Count();
+    }
+}
