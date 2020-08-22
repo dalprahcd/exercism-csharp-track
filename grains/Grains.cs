20083f52218e514bd690efcb1b61ccbe14c363d7
dalprahcd <dalpra.hcd@gmail.com>
2020-08-22 11:16:19 -0300

    CHANGED: Great improvement to take note of.

--- grains/Grains.cs
+++ grains/Grains.cs
@@ -4,28 +4,13 @@ public static class Grains
 {
     public static ulong Square(int n)
     {
-        if (n <= 0 || n > 64)
+        if (n < 1 || n > 64)
         {
             throw new ArgumentOutOfRangeException(nameof(n));
         }
 
-        return Power(2UL, (ulong)(n - 1));
+        return 1UL << (n - 1);
     }
 
-    public static ulong Total()
-    {
-        return (Power(2UL, 64UL) - 1);
-    }
-
-    private static ulong Power(ulong value, ulong factor)
-    {
-        ulong power = 1;
-
-        for (ulong i = 0; i < factor; i++)
-        {
-            power *= value;
-        }
-
-        return power;
-    }
+    public static ulong Total() => ulong.MaxValue;
 }
