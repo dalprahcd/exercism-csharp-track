192886449427611c2b71ad3e1cdb3656bd943289
dalprahcd <dalpra.hcd@gmail.com>
2020-08-22 08:49:08 -0300

    ADDED: ResistorColor exercise solution

---
+++ resistor-color/ResistorColor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+public static class ResistorColor
+{
+    private enum RESISTOR_BANDS
+    {
+        Black = 0,
+        Brown,
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        Violet,
+        Grey,
+        White
+    }
+
+
+    public static int ColorCode(string color)
+    {
+        Enum.TryParse(color, true, out RESISTOR_BANDS result);
+        return (int) result;
+    }
+
+    public static string[] Colors()
+    {
+        return Enum
+                .GetNames(typeof(RESISTOR_BANDS))
+                .Select(s => s.ToLower())
+                .ToArray();
+    }
+}
