fb61a7165f96478f52541a0cced10714db8df329
dalprahcd <dalpra.hcd@gmail.com>
2020-08-22 09:22:00 -0300

    ADDED: Pangram exercise solution.

---
+++ pangram/Pangram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+    private const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public static bool IsPangram(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return false;
+        }
+
+        return alphabet.All(
+            s => input.Contains(s, StringComparison.OrdinalIgnoreCase));
+    }
+}
