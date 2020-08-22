2e818b2ce1faaccb3b074e551d8302e3b87f88de
dalprahcd <dalpra.hcd@gmail.com>
2020-08-22 10:32:30 -0300

    ADDED: Acronym exercise solution.

---
+++ acronym/Acronym.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+public static class Acronym
+{
+    private static readonly char[] separators = {' ', ',', '/', '-', '_'};
+
+    public static string Abbreviate(string phrase)
+    {
+        if (string.IsNullOrEmpty(phrase))
+        {
+            return string.Empty;
+        }
+
+        return new string(
+            phrase.Split(separators, StringSplitOptions.RemoveEmptyEntries)
+            .Select(s => s.First())
+            .ToArray()
+        ).ToUpper();
+    }
+}
