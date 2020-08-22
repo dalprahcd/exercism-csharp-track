f9663ff933f4fce11f8bd545b24fe079e24efd60
dalprahcd <dalpra.hcd@gmail.com>
2020-08-22 12:10:48 -0300

    ADDED: PerfectNumbers exercise solution.

---
+++ perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        if (number < 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(number));
+        }
+
+        int aliquotSum = Enumerable.Range(1, number / 2)
+                            .Where(x => number % x == 0)
+                            .Sum();
+
+
+        if (aliquotSum > number)
+        {
+            return Classification.Abundant;
+        }
+        
+        if (aliquotSum < number)
+        {
+            return Classification.Deficient;
+        }
+
+        return Classification.Perfect;
+    }
+}
