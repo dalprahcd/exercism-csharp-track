89e42bbd43d15f7777ee3eee334472a44275623b
dalprahcd <dalpra.hcd@gmail.com>
2020-08-23 10:30:52 -0300

    ADDED: RobotName exercise solution.

---
+++ robot-name/RobotName.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Robot
+{
+    private static readonly Random random = new Random();
+    private static readonly HashSet<string> existingNames = new HashSet<string>();
+
+    public Robot()
+    {
+        Reset();
+    }
+
+    public string Name { get; private set;}
+
+    public bool Reset()
+    {
+        // There is no new possible names.
+        if (existingNames.Count == 26*26*999 - 1) // Not sure about the maximum number
+        {
+            return false;
+        }
+
+        StringBuilder nameBuilder = new StringBuilder();
+
+        do
+        {
+            nameBuilder.Clear();
+            nameBuilder
+                .AppendFormat("{0}", (char)random.Next('A','Z'))
+                .AppendFormat("{0}", (char)random.Next('A','Z'))
+                .AppendFormat("{0:000}", random.Next(0, 999));
+
+        } while (!existingNames.Add(nameBuilder.ToString()));
+
+        Name = nameBuilder.ToString();
+        return true;
+    }
+}
