70e2f7af5bc102292b4c7c19f468cdeb99fe0c3a
dalprahcd <dalpra.hcd@gmail.com>
2020-08-23 09:14:04 -0300

    ADDED: Hamming exercise solution.

---
+++ hamming/Hamming.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Hamming
+{
+    public static int Distance(string firstStrand, string secondStrand)
+    {
+        if (firstStrand == null || secondStrand == null)
+        {
+            throw new ArgumentException();
+        }
+
+        if (firstStrand.Length != secondStrand.Length)
+        {
+            throw new ArgumentException();
+        }
+
+        int hammingDistance = 0;
+        for (int i = 0; i < firstStrand.Length; i++)
+        {
+            if (firstStrand[i] != secondStrand[i])
+            {
+                hammingDistance++;
+            }
+        }
+
+        return hammingDistance;
+    }
+}
