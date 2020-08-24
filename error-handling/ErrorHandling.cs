5e716c3526ab300f2590964c441ee51ca8dcc695
dalprahcd <dalpra.hcd@gmail.com>
2020-08-24 20:51:12 -0300

    ADDED: ErrorHandling exercise solution.

---
+++ error-handling/ErrorHandling.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException() =>
+        throw new Exception("Error message");
+    
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        try
+        {
+            return int.Parse(input);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result) =>
+        int.TryParse(input, out result);
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        using (disposableObject)
+        {
+            throw new Exception("Error message");
+        }
+    }
+}
