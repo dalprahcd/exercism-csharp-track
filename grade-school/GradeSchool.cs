8b7197d4941805062942fc37a2c670eca4cc3668
dalprahcd <dalpra.hcd@gmail.com>
2020-08-24 10:01:36 -0300

    ADDED: GradeSchool exercise solution.

---
+++ grade-school/GradeSchool.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GradeSchool
+{
+    private readonly Dictionary<string, int> schoolRoster = new Dictionary<string, int>();
+
+    public void Add(string student, int grade)
+    {
+        if (schoolRoster.ContainsKey(student))
+        {
+            schoolRoster[student] = grade;
+        }
+        else 
+        {
+            schoolRoster.Add(student, grade);
+        }
+    }
+
+    public IEnumerable<string> Roster() => 
+        schoolRoster
+            .OrderBy(k => k.Value)
+            .ThenBy(k => k.Key)
+            .Select(k => k.Key);
+    
+
+    public IEnumerable<string> Grade(int grade) =>
+        schoolRoster
+            .Where(k => k.Value == grade)
+            .Select(k => k.Key)
+            .OrderBy(s => s);
+}
