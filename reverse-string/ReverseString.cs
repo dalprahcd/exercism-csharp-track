c0207d5893afb3e2749e2b252c99a6edcacaab5f
dalprahcd <dalpra.hcd@gmail.com>
2020-08-25 19:46:02 -0300

    ADDED: Reverse string exercise solution.

---
+++ reverse-string/ReverseString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//using System.Linq;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        if (input == null)
+        {
+            throw new ArgumentNullException(nameof(input));
+        }
+
+        char[] reversed = input.ToCharArray();
+        Array.Reverse(reversed);
+        return new string(reversed);
+
+        //return new string(input.Reverse().ToArray());
+    }
+}
