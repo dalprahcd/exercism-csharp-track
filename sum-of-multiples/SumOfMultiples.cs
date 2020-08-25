a7bddc9f6d0766db4c864cd142a542b21d043c51
dalprahcd <dalpra.hcd@gmail.com>
2020-08-25 20:57:40 -0300

    ADDED: SumOfMultiple exercise solution.

---
+++ sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        var naturalDividers = 
+            from n in Enumerable.Range(1, max - 1)
+            from m in multiples
+            where m != 0 && n % m == 0
+            select n;
+
+        return naturalDividers.Distinct().Sum();
+    }
+    
+    // I rather read the query above.
+    /*
+    public static int Sum(IEnumerable<int> multiples, int max) => 
+        Enumerable
+            .Range(1, max - 1)
+            .SelectMany(n => multiples
+                                .Where(m => m != 0 && n % m == 0)
+                                .Select(m => n))
+            .Distinct()
+            .Sum();
+    //*/
+}
