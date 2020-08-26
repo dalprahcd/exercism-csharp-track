f58ca9840983be381688380be29ca735b71f6e2b
dalprahcd <dalpra.hcd@gmail.com>
2020-08-26 12:06:34 -0300

    ADDED: DifferenceOfSquares exercise solution.

---
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Linq;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max)
+    {
+        var sum = Enumerable.Range(1, max).Sum();
+        return sum * sum;
+    }
+
+    public static int CalculateSumOfSquares(int max) =>
+        Enumerable.Range(1, max).Sum(x => x * x);
+
+    public static int CalculateDifferenceOfSquares(int max)/* =>
+        CalculateSquareOfSum(max) - CalculateSumOfSquares(max);//*/
+    {
+        // Let's not iterate two times
+        var sum = 0;
+        var sumSquare = 0;
+
+        for (int i = 1; i < max + 1; i++)
+        {
+            sum += i;
+            sumSquare += i * i;
+        }
+
+        return (sum * sum) - sumSquare;
+    }
+}
