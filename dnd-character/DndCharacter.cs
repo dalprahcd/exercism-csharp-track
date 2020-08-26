914d8ee67db93d2de2b3f1cd077ff5f3a5d182a1
dalprahcd <dalpra.hcd@gmail.com>
2020-08-26 17:45:51 -0300

    ADDED: DndCharacter exercise solution.

---
+++ dnd-character/DndCharacter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+public class DndCharacter
+{
+    private static readonly Random rnd  = new Random();
+
+    public int Strength { get; }        = Ability();
+    public int Dexterity { get; }       = Ability();
+    public int Constitution { get; }    = Ability();
+    public int Intelligence { get; }    = Ability();
+    public int Wisdom { get; }          = Ability();
+    public int Charisma { get; }        = Ability();
+    public int Hitpoints                => 10 + Modifier(Constitution);
+
+    public static int Modifier(int score) =>
+        (int)Math.Floor((score - 10) / 2.0d);
+
+    public static int Ability() =>
+        Enumerable
+            .Range(0, 4)
+            .Select(x => rnd.Next(1, 7))
+            .OrderByDescending(x => x)
+            .Take(3)
+            .Sum();
+
+    public static DndCharacter Generate() =>
+        new DndCharacter();
+}
