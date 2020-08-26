0b2d1c4dac4018ab52a3e5b36dc620c2696cc654
dalprahcd <dalpra.hcd@gmail.com>
2020-08-26 08:37:10 -0300

    ADDED: Series exercise solution.

---
+++ series/Series.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Series
+{
+    public static IEnumerable<string> Slices(string numbers, int sliceLength)
+    {
+        if (string.IsNullOrEmpty(numbers))
+        {
+            throw new ArgumentException(nameof(numbers));
+        }
+
+        if (sliceLength < 1 || sliceLength > numbers.Length)
+        {
+            throw new ArgumentException(nameof(sliceLength));
+        }
+
+        return Enumerable
+                    .Range(0, numbers.Length - sliceLength + 1)
+                    .Select(i => numbers.Substring(i, sliceLength));
+    }
+}
