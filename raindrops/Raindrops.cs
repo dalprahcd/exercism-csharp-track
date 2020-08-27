ee3db5771e1c7354b5f8d4037954ad3808ea32a7
dalprahcd <dalpra.hcd@gmail.com>
2020-08-27 14:43:01 -0300

    ADDED: Raindrops exercise solution.

---
+++ raindrops/Raindrops.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class Raindrops
+{
+    public static string Convert(int number)
+    {
+        IEnumerable<int> factors = Enumerable
+                                        .Range(1, number)
+                                        .Where(n => number % n == 0);
+
+        if (factors.All(f => f != 3 && f != 5 && f != 7))
+        {
+            return number.ToString();
+        }
+        
+        StringBuilder rainsDropBuilder = new StringBuilder();
+
+        if (factors.Contains(3))
+        {
+            rainsDropBuilder.Append("Pling");
+        }
+
+        if (factors.Contains(5))
+        {
+            rainsDropBuilder.Append("Plang");
+        }
+
+        if (factors.Contains(7))
+        {
+            rainsDropBuilder.Append("Plong");
+        }
+
+        return rainsDropBuilder.ToString();
+    }
+}
