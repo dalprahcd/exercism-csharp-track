b585a65f5a3498f56228518e6b3f99a54299c9b3
Hudson C. Dalprá <dalpra.hcd@gmail.com>
2020-08-28 19:22:21 -0300

    ADDED: BinarySearch exercise solution.

---
+++ binary-search/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercism.CSharp.Solutions.BinarySearchExercise
+{
+    public static class BinarySearch
+    {
+        public static int Find(int[] input, int value)
+        {
+            int low = 0;
+            int high = input.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = (high + low) / 2;
+
+                if (input[mid] > value)
+                {
+                    high = mid - 1;
+                }
+                else if (input[mid] < value)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
