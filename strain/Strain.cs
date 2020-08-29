04d3c7b4644c75a8eaa187111d270c530024452d
Hudson C. Dalprá <dalpra.hcd@gmail.com>
2020-08-29 12:10:57 -0300

    ADDED: Strain exercise solution.

---
+++ strain/Strain.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+namespace Exercism.CSharp.Solutions.StrainExercise
+{
+    public static class Strain
+    {
+        public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            foreach (var item in collection)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
+        {
+            foreach (var item in collection)
+            {
+                if (!predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
