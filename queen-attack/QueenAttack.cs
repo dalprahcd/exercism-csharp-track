d62362e4b1df88a18a6c30ecaef6e684b989ba2b
Hudson Dalprá <dalpra.hcd@gmail.com>
2020-08-30 15:22:35 -0300

    ADDED: QueenAttack exercise solution.

---
+++ queen-attack/QueenAttack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercism.CSharp.Solutions.QueenAttackExercise
+{
+    public class Queen
+    {
+        public Queen(int row, int column)
+        {
+            if (row < 0 || row > 7)
+            {
+                throw new ArgumentOutOfRangeException(nameof(row));
+            }
+
+            if (column < 0 || column > 7)
+            {
+                throw new ArgumentOutOfRangeException(nameof(column));
+            }
+
+            Row = row;
+            Column = column;
+        }
+
+        public int Row { get; }
+        public int Column { get; }
+    }
+
+    public static class QueenAttack
+    {
+        public static bool CanAttack(Queen white, Queen black) =>
+            white.Row == black.Row ||
+            white.Column == black.Column ||
+            Math.Abs(white.Row - black.Row) == Math.Abs(white.Column - black.Column);
+
+        public static Queen Create(int row, int column) =>
+            new Queen(row, column);
+    }
+}
