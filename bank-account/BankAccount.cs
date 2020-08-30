e116066a58317b7ec7f895162295e594cc2aad9d
Hudson C. Dalprá <dalpra.hcd@gmail.com>
2020-08-30 20:51:08 -0300

    ADDED: BankAccount exercise solution.

---
+++ bank-account/BankAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Exercism.CSharp.Solutions.BankAccountExercise
+{
+    public class BankAccount
+    {
+        private readonly object syncLock = new object();
+        private bool isClosed;
+        private decimal balance;
+
+        public void Open()
+        {
+            lock (syncLock)
+            {
+                isClosed = false;
+            }
+        }
+
+        public void Close()
+        {
+            lock (syncLock)
+            {
+                isClosed = true;
+            }
+        }
+
+        public decimal Balance
+        {
+            get
+            {
+                lock (syncLock)
+                {
+                    if (isClosed)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                    return balance;
+                }
+            }
+        }
+
+        public void UpdateBalance(decimal change)
+        {
+            lock (syncLock)
+            {
+                if (isClosed)
+                {
+                    throw new InvalidOperationException();
+                }
+                balance += change;
+            }
+        }
+    }
+}
