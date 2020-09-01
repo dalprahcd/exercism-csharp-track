eb97ec802e3cba568fa9cb4f5c04295348571404
Hudson C. Dalprá <dalpra.hcd@gmail.com>
2020-09-01 12:15:38 -0300

    ADDED: CircularBuffer exercise solution.

---
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Exercism.CSharp.Solutions.CircularBufferExercise
+{
+    public class CircularBuffer<T>
+    {
+        private readonly T[] buffer;
+        private int read;
+        private int write;
+        private int items;
+
+        public CircularBuffer(int capacity)
+        {
+            if (capacity < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(capacity));
+            }
+
+            buffer = new T[capacity];
+            read = write = items = 0;
+        }
+
+        public T Read()
+        {
+            if (items == 0)
+            {
+                throw new InvalidOperationException("Buffer empty!");
+            }
+
+            T value = buffer[read];
+            read = (read + 1) % buffer.Length;
+            items--;
+
+            return value;
+        }
+
+        public void Write(T value)
+        {
+            if (items == buffer.Length)
+            {
+                throw new InvalidOperationException("Buffer full!");
+            }
+
+            buffer[write] = value;
+            write = (write + 1) % buffer.Length;
+            items++;
+        }
+
+        public void Overwrite(T value)
+        {
+            if (items < buffer.Length)
+            {
+                Write(value);
+            }
+            else
+            {
+                buffer[write] = value;
+                write = (write + 1) % buffer.Length;
+                read = (read + 1) % buffer.Length;
+            }
+        }
+
+        public void Clear()
+        {
+            items = 0;
+            read = write;
+        }
+    }
+}
