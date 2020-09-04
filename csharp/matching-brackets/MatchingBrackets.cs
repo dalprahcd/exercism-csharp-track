18e4070cb98cbabf34fd85bb66dcbae3cb274d56
Hudson Dalprá <dalpra.hcd@gmail.com>
2020-09-04 14:18:36 -0300

    ADDED: MatchingBrackets exercise solution.

---
+++ csharp/matching-brackets/MatchingBrackets.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercism.CSharp.Solutions.MatchingBracketsExercise
+{
+    public static class MatchingBrackets
+    {
+        private static readonly Dictionary<char, char> wrapperPairs = new Dictionary<char, char>
+        {
+            ['('] = ')',
+            ['['] = ']',
+            ['{'] = '}'
+        };
+
+        public static bool IsPaired(string input)
+        {
+            var wrappersToCheck = new Stack<char>();
+
+            foreach (var wrapper in input.Where(IsWrapper))
+            {
+                if (wrapperPairs.Keys.Contains(wrapper))
+                {
+                    // It is a opening wrapper, add to the stack.
+                    wrappersToCheck.Push(wrapper);
+                }
+                else if (wrappersToCheck.Count > 0)
+                {
+                    // It is a closing wrapper with existing opening wrapper on the stack
+                    // check if it is the correct pair
+                    if (!wrappersToCheck.Peek().IsCorrectPair(wrapper))
+                    {
+                        return false;
+                    }
+
+                    // remove this opening wrapper from the stack
+                    _ = wrappersToCheck.Pop();
+                }
+                else
+                {
+                    // It is a closing wrapper without a opening wrapper.
+                    return false;
+                }
+            }
+
+            // All opening wrappers should have been matched.
+            return wrappersToCheck.Count == 0;
+        }
+
+        private static bool IsWrapper(this char ch) =>
+            wrapperPairs.Keys.Contains(ch) || wrapperPairs.Values.Contains(ch);
+
+        private static bool IsCorrectPair(this char opening, char closing) =>
+            wrapperPairs[opening] == closing;
+    }
+}
---
+++ csharp/matching-brackets/MatchingBracketsTests.cs
@@ -0,0 +1,128 @@
+// This file was auto-generated based on version 2.0.0 of the canonical data.
+
+using Xunit;
+
+namespace Exercism.CSharp.Solutions.MatchingBracketsExercise.Tests
+{
+    public class MatchingBracketsTests
+    {
+        [Fact]
+        public void Paired_square_brackets()
+        {
+            const string value = "[]";
+            Assert.True(MatchingBrackets.IsPaired(value));
+        }
+
+        [Fact]
+        public void Empty_string()
+        {
+            const string value = "";
+            Assert.True(MatchingBrackets.IsPaired(value));
+        }
+
+        [Fact]
+        public void Unpaired_brackets()
+        {
+            const string value = "[[";
+            Assert.False(MatchingBrackets.IsPaired(value));
+        }
+
+        [Fact]
+        public void Wrong_ordered_brackets()
+        {
+            const string value = "}{";
+            Assert.False(MatchingBrackets.IsPaired(value));
+        }
+
+        [Fact]
+        public void Wrong_closing_bracket()
+        {
+            const string value = "{]";
+            Assert.False(MatchingBrackets.IsPaired(value));
+        }
+
+        [Fact]
+        public void Paired_with_whitespace()
+        {
+            const string value = "{ }";
+            Assert.True(MatchingBrackets.IsPaired(value));
+        }
+
+        [Fact]
+        public void Partially_paired_brackets()
+        {
+            const string value = "{[])";
+            Assert.False(MatchingBrackets.IsPaired(value));
+        }
+
+        [Fact]
+        public void Simple_nested_brackets()
+        {
+            const string value = "{[]}";
+            Assert.True(MatchingBrackets.IsPaired(value));
+        }
+
+        [Fact]
+        public void Several_paired_brackets()
+        {
+            const string value = "{}[]";
+            Assert.True(MatchingBrackets.IsPaired(value));
+        }
+
+        [Fact]
+        public void Paired_and_nested_brackets()
+        {
+            const string value = "([{}({}[])])";
+            Assert.True(MatchingBrackets.IsPaired(value));
+        }
+
+        [Fact]
+        public void Unopened_closing_brackets()
+        {
+            const string value = "{[)][]}";
+            Assert.False(MatchingBrackets.IsPaired(value));
+        }
+
+        [Fact]
+        public void Unpaired_and_nested_brackets()
+        {
+            const string value = "([{])";
+            Assert.False(MatchingBrackets.IsPaired(value));
+        }
+
+        [Fact]
+        public void Paired_and_wrong_nested_brackets()
+        {
+            const string value = "[({]})";
+            Assert.False(MatchingBrackets.IsPaired(value));
+        }
+
+        [Fact]
+        public void Paired_and_incomplete_brackets()
+        {
+            const string value = "{}[";
+            Assert.False(MatchingBrackets.IsPaired(value));
+        }
+
+        [Fact]
+        public void Too_many_closing_brackets()
+        {
+            const string value = "[]]";
+            Assert.False(MatchingBrackets.IsPaired(value));
+        }
+
+        [Fact]
+        public void Math_expression()
+        {
+            const string value = "(((185 + 223.85) * 15) - 543)/2";
+            Assert.True(MatchingBrackets.IsPaired(value));
+        }
+
+        [Fact]
+        public void Complex_latex_expression()
+        {
+            const string value = @"\left(\begin{array}{cc} \frac{1}{3} & x\\ \mathrm{e}^{x} &... x^2 \end{array}\right)";
+            Assert.True(MatchingBrackets.IsPaired(value));
+        }
+    }
+}
