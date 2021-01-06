7ac263f9d957440b32f6792b19fb32d255ae193e
Hudson C. Dalprá <dalpra.hcd@gmail.com>
2021-01-06 09:46:00 -0300

    ADDED: PascalsTriangle exercise solution.

---
+++ csharp/pascals-triangle/PascalsTriangle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Exercism.CSharp.Solutions.PascalsTriangleExercise
+{
+    public static class PascalsTriangle
+    {
+        public static IEnumerable<IEnumerable<int>> Calculate(int lines)
+        {
+            for (int line = 1; line <= lines; line++)
+            {
+                yield return CalculateLine(line);
+            }
+        }
+
+        private static IEnumerable<int> CalculateLine(int line)
+        {
+            int c = 1;
+
+            for (int i = 1; i <= line; i++)
+            {
+                yield return c;
+
+                c = (c * (line - i)) / i;
+            }
+        }
+    }
+}
