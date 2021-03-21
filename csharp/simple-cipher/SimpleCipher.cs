a1ac79370d66dd7aa88e85c888d93fb5803dcbb5
Hudson C. Dalprá <dalpra.hcd@gmail.com>
2021-03-21 08:27:54 -0300

    ADDED: SimpleCipher csharp exercise solution.

---
+++ csharp/simple-cipher/SimpleCipher.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Linq;
+
+namespace Exercism.CSharp.Solutions.SimpleCipherExercise
+{
+    public class SimpleCipher
+    {
+        private readonly int[] rotate;
+
+        public SimpleCipher()
+        {
+            var rnd = new Random();
+
+            rotate = Enumerable
+                        .Range(0, 100)
+                        .Select(_ => rnd.Next(0, 25))
+                        .ToArray();
+
+            Key = new string(rotate.Select(i => (char)('a' + i)).ToArray());
+        }
+
+        public SimpleCipher(string key)
+        {
+            rotate = key.Select(c => c - 'a').ToArray();
+
+            Key = key;
+        }
+
+        public string Key { get; }
+
+        public string Encode(string plaintext)
+        {
+            var encoded = new char[plaintext.Length];
+
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                encoded[i] = Shift(plaintext[i], rotate[i % rotate.Length]);
+            }
+
+            return new string(encoded);
+        }
+
+        public string Decode(string ciphertext)
+        {
+            var decoded = new char[ciphertext.Length];
+
+            for (int i = 0; i < ciphertext.Length; i++)
+            {
+                decoded[i] = Shift(ciphertext[i], -rotate[i % rotate.Length]);
+            }
+
+            return new string(decoded);
+        }
+
+        public char Shift(int letter, int shift)
+        {
+            var output = letter + shift;
+
+            if (output < 'a')
+            {
+                var diff = 'a' - output - 1;
+
+                return (char)('z' - diff);
+            }
+
+            if (output > 'z')
+            {
+                var diff = output - 'z' - 1;
+
+                return (char)('a' + diff);
+            }
+
+            return (char)output;
+        }
+    }
+}
