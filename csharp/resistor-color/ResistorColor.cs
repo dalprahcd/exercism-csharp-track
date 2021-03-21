c25804ff00be4546b1bf4351f14c90df9c83d0ae
Hudson C. Dalprá <dalpra.hcd@gmail.com>
2021-03-21 11:11:16 -0300

    CHANGED: ResistorColor exercise using string.

--- csharp/resistor-color/ResistorColor.cs
+++ csharp/resistor-color/ResistorColor.cs
@@ -1,33 +1,14 @@
 ﻿using System;
-using System.Linq;
 
 namespace Exercism.CSharp.Solutions.ResistorColorExercise
 {
     public static class ResistorColor
     {
-        private enum ResistorBands
-        {
-            Black = 0,
-            Brown,
-            Red,
-            Orange,
-            Yellow,
-            Green,
-            Blue,
-            Violet,
-            Grey,
-            White
-        }
+        private static readonly string[] colors = new[]
+            { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
 
-        public static int ColorCode(string color)
-        {
-            Enum.TryParse(color, true, out ResistorBands result);
-            return (int)result;
-        }
+        public static int ColorCode(string color) => Array.IndexOf(colors, color);
 
-        public static string[] Colors() =>
-            Enum.GetNames(typeof(ResistorBands))
-                .Select(s => s.ToLower())
-                .ToArray();
+        public static string[] Colors() => colors;
     }
 }
