b4df24c7617f09d9071898d6d2351f95a8319642
Hudson C. Dalprá <dalpra.hcd@gmail.com>
2021-04-04 09:09:11 -0300

    ADDED: CSharp ParallelLetterFrequency exercise solution

---
+++ csharp/parallel-letter-frequency/ParallelLetterFrequency.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercism.CSharp.Solutions.ParallelLetterFrequencyExercise
+{
+    public static class ParallelLetterFrequency
+    {
+        public static Dictionary<char, int> Calculate(IEnumerable<string> texts)
+        {
+            var letterFrequency = new ConcurrentDictionary<char, int>();
+
+            Parallel.ForEach(
+                texts.Where(text => !string.IsNullOrEmpty(text)).Select(text => text.ToLower()),
+                text =>
+                {
+                    foreach (var letter in text.Where(char.IsLetter))
+                    {
+                        letterFrequency.AddOrUpdate(
+                            letter,
+                            1,
+                            (_, oldValue) => ++oldValue);
+                    }
+                });
+
+            return new Dictionary<char, int>(letterFrequency);
+        }
+    }
+}
