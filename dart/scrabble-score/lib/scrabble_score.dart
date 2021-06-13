1a375bcfddd13aa3f917519356b2eec1c7456eaf
Hudson <dalpra.hcd@gmail.com>
2021-06-13 16:29:55 -0300

    ADDED: dart scrabble_score exercise solution.

---
+++ dart/scrabble-score/lib/scrabble_score.dart
@@ -0,0 +1,39 @@
+final _letterPointsMap = const {
+  'A': 1,
+  'B': 3,
+  'C': 3,
+  'D': 2,
+  'E': 1,
+  'F': 4,
+  'G': 2,
+  'H': 4,
+  'I': 1,
+  'J': 8,
+  'K': 5,
+  'L': 1,
+  'M': 3,
+  'N': 1,
+  'O': 1,
+  'P': 3,
+  'Q': 10,
+  'R': 1,
+  'S': 1,
+  'T': 1,
+  'U': 1,
+  'V': 4,
+  'W': 4,
+  'X': 8,
+  'Y': 4,
+  'Z': 10
+};
+
+int score(String word) {
+  int sum = 0;
+
+  for (var i = 0; i < word.length; i++) {
+    var letter = word[i].toUpperCase();
+    sum += _letterPointsMap[letter];
+  }
+
+  return sum;
+}
