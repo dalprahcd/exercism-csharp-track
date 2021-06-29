94785cac0080270793d6e195bbb0948abee2a43c
Hudson <dalpra.hcd@gmail.com>
2021-06-29 13:43:15 -0300

    REMOVED: C track, focusing on high level languages for now.

--- c/hamming/src/hamming.h
+++
@@ -1,6 +0,0 @@
-#ifndef HAMMING_H
-#define HAMMING_H
-
-long long compute(const char *lhs, const char *rhs);
-
-#endif
--- c/resistor-color-duo/src/resistor_color_duo.c
+++
@@ -1,6 +0,0 @@
-#include "resistor_color_duo.h"
-
-uint16_t color_code(const resistor_band_t *bands)
-{
-    return 10 * bands[0] + bands[1];
-}
